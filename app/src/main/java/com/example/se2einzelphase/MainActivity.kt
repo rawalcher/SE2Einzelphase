064c7ae133cc63055f0d5272b0e2438298672c8d
Tocuro <rawalcher@edu.aau.at>
2024-03-05 14:44:42 +0100

    colors

--- app/src/main/java/com/example/se2einzelphase/MainActivity.kt
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.kt
@@ -11,9 +11,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.se2einzelphase.ui.theme.SE2EinzelphaseTheme
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -23,6 +23,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 
 import androidx.compose.ui.unit.dp
@@ -31,11 +32,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SE2EinzelphaseTheme {
+            SE2EinzelphaseTheme{
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Green
                 ) {
                     val outputState = remember { mutableStateOf("") }
 
@@ -85,8 +86,14 @@ fun OutputField(text: String) {
     }
 }
 
+// Aufgabe 12210093 % 7 = 0
+// 0 - Ziffern der Größe nach sortieren, Primzahlen werden gestrichen
 fun processLocal(number: String): String {
-    return "Processed: $number"
+    // array with primes because a method to calculate would be overkill
+
+    // max heap to sort it descending
+
+    return "Locally Processed: $number"
 }
 
 fun processServer(number: String): String {
@@ -95,7 +102,12 @@ fun processServer(number: String): String {
 
 @Composable
 fun StyledButton(text: String, onClick: () -> String, outputState: MutableState<String>) {
-    Button(onClick = { outputState.value = onClick() }, modifier = Modifier.padding(8.dp)) {
+    Button(
+        onClick = { outputState.value = onClick() },
+        modifier = Modifier.padding(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = androidx.compose.ui.graphics.Color.Yellow,
+            contentColor = androidx.compose.ui.graphics.Color.Blue)) {
         Text(text)
     }
 }
@@ -108,8 +120,8 @@ fun TwoButtons(outputState: MutableState<String>) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Row(horizontalArrangement = Arrangement.Center) {
-                StyledButton("Send Number", { processLocal("1234") }, outputState)
-                StyledButton("Process Number", { processServer("5678") }, outputState)
+                StyledButton("Process Remote", { processServer("1234") }, outputState)
+                StyledButton("Process Local", { processLocal("5678") }, outputState)
             }
         }
     }
